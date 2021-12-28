4ca4775786c44a376d5963e1eb4e2e4f37941c03
Shidiq <shidiq.hidayat@gmail.com>
2021-12-28 11:47:56 +0700

    0.0.1-alpha.3

---
+++ snhlib/image.py
@@ -0,0 +1,43 @@
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+class Style:
+    @staticmethod
+    def reset():
+        matplotlib.rcParams.update(matplotlib.rcParamsDefault)
+
+    @staticmethod
+    def paper(loc='best', classic=True, figsize=[10.72, 8.205]):
+        if classic:
+            plt.style.use('classic')
+        params = {
+            "axes.formatter.useoffset": False,
+            "font.family": "sans-serif",
+            "font.sans-serif": "Arial",
+            "xtick.labelsize": 28,
+            "ytick.labelsize": 28,
+            "axes.labelsize": 28,
+            "axes.labelweight": "bold",
+            'axes.titlesize': 28,
+            'axes.titleweight': "bold",
+            "figure.dpi": 150,
+            "figure.figsize": figsize,
+            "legend.loc": loc,
+            "legend.fontsize": 24,
+            "legend.fancybox": True,
+            "mathtext.fontset": 'custom',
+            "mathtext.default": 'regular',
+            "figure.autolayout": True,
+            "patch.edgecolor": "#000000",
+            "text.color": "#000000",
+            "axes.edgecolor": "#000000",
+            "axes.labelcolor": "#000000",
+            "xtick.color": "#000000",
+            "ytick.color": "#000000",
+        }
+        matplotlib.rcParams.update(params)
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        return fig, ax
