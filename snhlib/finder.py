f9fce14fc99cd302881dd0ff4eb09ce451a174e9
Shidiq <shidiq.hidayat@gmail.com>
2022-01-03 17:12:43 +0700

    0.0.1-alpha.13

--- snhlib/finder.py
+++ snhlib/finder.py
@@ -18,11 +18,16 @@ class FinderScan:
         return res, (path, subdirs, files)
 
     @staticmethod
-    def openDataGeNose(item, cols):
+    def openDataGeNose(item, cols=None):
+        if cols is None:
+            cols = ['time(s)'] + \
+                [f'S{i + 1}' for i in range(10)] + ['Temp', 'Humid']
+
         if item.find('.csv') != -1:
             data = pd.read_csv(item)
         else:
             data = json.load(open(item, 'r'))
             data = data['datasensor']
             data = pd.DataFrame(data, columns=cols)
+
         return data
